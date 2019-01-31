30941726ca86efdd2d3b91538ad36d0c173f3bb1
Anderson Amorim <andersonamr@outlook.com>
2019-01-31 18:35:31 -0300

    Create Question1.py

---
+++ Exercises/Question1.py
@@ -0,0 +1,4 @@
+# Make a Program that asks for a number and then displays the message The number entered was [number].
+
+number = int(input("Write a number: "))
+print("The number you wrote was: {}".format(number))
