ccb9a944b472ae8da51bf8baa04eb06cd0c71b27
Anderson Amorim <andersonamr@outlook.com>
2019-01-31 20:45:11 -0300

    Create question3.py

---
+++ Exercises/question3.py
@@ -0,0 +1,8 @@
+#Faça um Programa que converta metros para centímetros.
+
+print("Nesse programa você poderá converter metros para centímetros.")
+meters = float(input("Digite quantos metros você quer converter: "))
+
+res = meters * 100
+
+print("{} metros são {} centímetros".format(meters, res))
